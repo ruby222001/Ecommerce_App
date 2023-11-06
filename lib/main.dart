3152958b8899190dde7c5b286ff5336353c625c2
ruby222001 <rubymaharzn@gmail.com>
2023-11-06 19:52:12 +0545

    ecommerce app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:app/pages/Intropage.dart';
+import 'package:app/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+     home: const IntroPage(),
+     theme: lightMode,
+      
+    );
+    
+    }}
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+     primary: Colors.grey.shade200, 
+     secondary: Colors.white, 
+    
+     background:Colors.grey.shade300, 
+     inversePrimary: Colors.grey.shade700,
+     ),
+);
