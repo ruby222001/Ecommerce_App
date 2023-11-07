628d4505bcb1d2f3d5ea5d59537b46b28826cfa5
ruby222001 <rubymaharzn@gmail.com>
2023-11-07 12:49:59 +0545

    intropage

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,51 @@
+import 'package:app/components/my_button.dart';
+import 'package:flutter/material.dart';
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //icon
+      Icon(
+        Icons.shopping_bag,
+        size: 70,
+      color: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      const SizedBox(height: 25),
+      //title
+      const Text(
+        'Minimal Shop',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+        ),
+      const SizedBox(height: 10),
+
+        //subtitle
+        Text("Premium Quality Products",
+        style: TextStyle(
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        ),
+
+      const SizedBox(height: 20),
+
+        //button
+MyButton(
+  onTap:() => Navigator.pushNamed(context, '/shop_page'),
+   child:const Icon(
+  Icons.arrow_forward
+),)
+      ],
+        ),
+      ),
+    );
+  }
+}
