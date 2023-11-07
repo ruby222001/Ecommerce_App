84e59f99c78865e1a7fb651a263acdd7ff54d8d5
ruby222001 <rubymaharzn@gmail.com>
2023-11-07 12:49:26 +0545

    cartpage

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('cart page'),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
+  }
+}
